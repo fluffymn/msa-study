bfcff33d3af09b85143d352a4c6aefe4b1ad8b7e
Mina Park <mineezm9@gmail.com>
2023-06-14 15:11:30 +0900

    Section 5 :  Users Microservice - 사용자 조회 api 구현
     - Users Microservice의 api에 prefix `/user-service` 설정
     - 사용자의 주문 목록 관련 VO `ResponseOrder` 생성
     - 전체 사용자 정보를 가져오는 api `GET /users` 구현
     - 사용자 아이디로 하나의 사용자 정보를 가져오는 api `GET /users/{userId}` 구현

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service/")
 public class UserController {
 
     private Environment env;
@@ -33,12 +33,12 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user-service/health_check")
+    @GetMapping("/health_check")
     public String status(){
         return String.format("It's Working in User Service on Port %s", env.getProperty("local.server.port"));
     }
 
-    @GetMapping("/user-service/welcome")
+    @GetMapping("/welcome")
     public String welcome(){
         return greeting.getMessage();
     }
@@ -53,5 +53,26 @@ public class UserController {
         ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<ResponseUser>> getUsers() {
+        Iterable<UserEntity> userList = userService.getUserByAll();
+
+        List<ResponseUser> result = new ArrayList<>();
+        userList.forEach( v -> {
+            result.add(new ModelMapper().map(v, ResponseUser.class));
+        });
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<ResponseUser> getUser(@PathVariable("userId") String userId) {
+        UserDto userDto = userService.getUserByUserId(userId);
+
+        ResponseUser result = new ModelMapper().map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
